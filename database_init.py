9a015adcb643edace4e04a1f3c98b4e0ab215b7e
alex.p <saska77@mail.ru>
2022-03-12 20:59:55 +0300

    create tables

--- database_init.py
+++ database_init.py
@@ -1,5 +1,4 @@
-from database import engine
+from database import engine, Base
 from models import Users, Statuses
 
-Users.Base.metadata.create_all(bind=engine)
-Statuses.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
